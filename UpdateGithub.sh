42bd33549576bda454ce9a7b65324d58d116d91a
mutek <mutek@inventati.org>
2015-05-28 09:29:36 +0200

    [751793348] aggiornato UpdateGithub.sh per integrare anche il versioning con gli annotated tags

--- UpdateGithub.sh
+++ UpdateGithub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# UpdateGithub.sh
+# UpdateGithub.sh v1.0
 #
 #
 # Copyleft )C( 2013-2015 Luca Cappelletti <luca.cappelletti@gmail.com>
@@ -25,19 +25,29 @@ git config --global user.name "mutek"
 git config --global user.email mutek@inventati.org
 
 commento=$1
+versione=$2
+
 [ -z $1 ] && commento=""
+
+# annota tags
 
 echo "Aggiungo i cambiamenti al database locale"
 git add -A .
-#echo "Inserisci una descrizione dei tuoi cambiamenti"
-#read n
+
 echo "Eseguo la finalizzazione dei cambiamenti in locale"
 EPOCA_UNIX=$(date +%N)
 COMMENTO="["$EPOCA_UNIX"] "$commento
+VERSIONE="v"$versione
+
 echo "Commento transazione: "$COMMENTO
+[ -z $2 ] || { echo"Versione: "$VERSIONE; git tag -a $VERSIONE -m "$COMMENTO"; }
+wait
 git commit -a -m "$COMMENTO"
+wait
 
 echo "Invio i cambiamenti locali al deposito remoto..."
 git push origin master
+wait
+git push origin --tags
 #git push origin gh-pages
 echo "ok"
