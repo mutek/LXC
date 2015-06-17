f141af09a4ac49ec4b751a1856f9fc5ebeba8389
mutek <mutek@inventati.org>
2015-06-17 02:14:43 +0200

    [642147871]

--- Supporto/mc_mysql/init-mysql.sh
+++ Supporto/mc_mysql/init-mysql.sh
@@ -24,7 +24,8 @@ cd /root
 
 # se passa oltre significa che il file della pwd non è presente, si prova a svolgere il setup
 MYSQL_RANDOM_PASSWORD="$(pwgen -s 25 1)"
-echo "MYSQL_RANDOM_PASSWORD = "$MYSQL_RANDOM_PASSWORD
+wait
+echo " init-mysql.sh: MYSQL_RANDOM_PASSWORD = "$MYSQL_RANDOM_PASSWORD
 echo $MYSQL_RANDOM_PASSWORD > mysql_rnd_pwd.txt
 mysqladmin -u root password $MYSQL_RANDOM_PASSWORD || { echo "ERRORE: il setup della pwd di root è fallito" ;exit; }
 wait
@@ -34,7 +35,7 @@ wait
 
 MYSQL_MAILUSER_PWD="$(pwgen -s 25 1)"
 wait
-echo "MYSQL_MAILUSER_PWD = "$MYSQL_MAILUSER_PWD
+echo " init-mysql.sh: MYSQL_MAILUSER_PWD = "$MYSQL_MAILUSER_PWD
 echo $MYSQL_MAILUSER_PWD > mysql_mailuser_pwd.txt
 
 ROOT_PWD="$(cat ./mysql_pwd.txt)"
@@ -44,6 +45,7 @@ DB_USER_PWD="$(cat ./mysql_mailuser_pwd.txt)"
 
 [ -f $DB_NAME.sql ] || { echo "ERRORE: non trovo il file "$DB_NAME".sql"; exit; }
 
+echo " init-mysql.sh: eseguo le chiamate mysql..."
 mysql -u root --password=$ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS "$DB_NAME";"
 wait
 
@@ -59,5 +61,5 @@ wait
 mysql -u root --password=$ROOT_PWD -e  "GRANT SELECT,INSERT,UPDATE,DELETE ON "$DB_NAME".* TO '"$DB_USER"'@'localhost';"
 wait
 
-echo "script init-mysql.sh end"
+echo " init-mysql.sh: script init-mysql.sh end"
 mysql -u root --password="$MYSQL_PWD" -e 'show databases;'
--- Supporto/mc_mysql/mysql.d/setup-database.sh
+++ Supporto/mc_mysql/mysql.d/setup-database.sh
@@ -8,7 +8,7 @@ do
 	[ -f ../$file_supporto ] || { echo "ERRORE: non trovo il file "$file_supporto" da trasferire al container"; exit; } 
 
 	# si presuppone di ereditare le variabili di ambiente dello script che chiama: make-container
-	echo " copio "$file_supporto" in "$LXC_CONFIG/$MC_NOME_CONTAINER"/rootfs/root/"
+	echo " setup-database.sh: copio "$file_supporto" in "$LXC_CONFIG/$MC_NOME_CONTAINER"/rootfs/root/"
 	
 	[ -d $LXC_CONFIG/$MC_NOME_CONTAINER/rootfs/root ] || { echo "ERRORE: non identifico la cartella: "$LXC_CONFIG/$MC_NOME_CONTAINER"/rootfs/root/"; exit; }
 	
@@ -18,22 +18,22 @@ do
 done
 
 # 1.1) avvio il container
-
-echo "Avvio il container per svolgere i lavori...attendi almeno 10 secondi per l'operazione"
+echo " setup-database.sh: Avvio il container per svolgere i lavori...attendi almeno 10 secondi per l'operazione"
 lxc-start -n $MC_NOME_CONTAINER
-wait
-sleep 10
+sleep 20
 
 # 1.2) reboot macchina
-echo "setup-database.sh: reboot container..."
-lxc-attach -n $MC_NOME_CONTAINER -- /sbin/reboot
-wait
-sleep 10
+echo " setup-database.sh: reboot container..."
+lxc-attach -n $MC_NOME_CONTAINER -- /sbin/poweroff
+sleep 20
+
+# 1.3) restart
+echo " setup-database.sh: start again..."
+lxc-start -n $MC_NOME_CONTAINER
+sleep 20
 
 # 2) crea root pwd
-
-echo "setup-database.sh: eseguo init-mysql.sh nel container..."
+echo " setup-database.sh: eseguo init-mysql.sh nel container..."
 lxc-attach -n $MC_NOME_CONTAINER -- /root/init-mysql.sh
 wait
 sleep 10
-
