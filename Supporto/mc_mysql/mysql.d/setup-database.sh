4cdfe7803113a738faca47bf4cb0378a0bca5999
mutek <mutek@inventati.org>
2015-06-17 23:03:02 +0200

    [039904171]

--- Supporto/mc_mysql/mysql.d/setup-database.sh
+++ Supporto/mc_mysql/mysql.d/setup-database.sh
@@ -25,7 +25,7 @@ sleep 20
 # 1.2) reboot macchina
 echo " setup-database.sh: stop container..."
 #lxc-attach -n $MC_NOME_CONTAINER -R --clear-env -- /sbin/poweroff
-lxc-stop -n $MC_NOME_CONTAINER
+lxc-stop -n $MC_NOME_CONTAINER -C
 wait
 sleep 10
 
@@ -34,8 +34,25 @@ echo " setup-database.sh: start again..."
 lxc-start -n $MC_NOME_CONTAINER -C
 sleep 20
 
+# 1.4) restart
+echo " setup-database.sh: start reboot..."
+lxc-attach -n $MC_NOME_CONTAINER -R --clear-env -- /sbin/reboot
+sleep 20
+
 # 2) crea root pwd
 echo " setup-database.sh: eseguo init-mysql.sh nel container..."
 lxc-attach -n $MC_NOME_CONTAINER -R --clear-env -- /root/init-mysql.sh
 wait
 sleep 10
+
+# 2.1) restart
+echo " setup-database.sh: start reboot 2..."
+lxc-attach -n $MC_NOME_CONTAINER -R --clear-env -- /sbin/reboot
+sleep 20
+
+# 2.2) crea root pwd
+echo " setup-database.sh: eseguo init-mysql.sh nel container 2..."
+lxc-attach -n $MC_NOME_CONTAINER -R --clear-env -- /root/init-mysql.sh
+wait
+sleep 10
+
