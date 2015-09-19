daa987c5e62ae58bf07b617ba71900bc2a99f4d9
mutek <mutek@inventati.org>
2015-09-19 10:42:01 +0200

    [087825364]

--- Supporto/mc_postino/container.d/0-build.sh
+++ Supporto/mc_postino/container.d/0-build.sh
@@ -462,6 +462,11 @@ mv /etc/opendkim.conf /etc/opendkim.conf.ORIGINAL
 wait
 cp /root/container.d/opendkim/etc/opendkim.conf /etc/
 cp /root/container.d/etc/default/opendkim /etc/default/opendkim
+wait
+
+# sostituisci MC_DOMINIO_CLIENTE
+sed -i "s/MC_DOMINIO_CLIENTE/$MC_DOMINIO_CLIENTE/g" /etc/opendkim.conf
+
 # genera la chiave e scrivi gli output in root
 cd /root
 # 1024 altrimenti i campi di text input web dei pannelli DNS che accettano tipicamente 255 caratteri sbroccano con chiavi da 4096
